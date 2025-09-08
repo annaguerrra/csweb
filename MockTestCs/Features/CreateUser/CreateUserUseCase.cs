6ce3431e9a11063aa9c7efaf9235a268abe2f688
Anna Guerra <annaguerracastro@gmail.com>
2025-09-08 01:44:24 -0300

    create user use case

--- MockTestCs/Features/CreateUser/CreateUserUseCase.cs
+++ MockTestCs/Features/CreateUser/CreateUserUseCase.cs
@@ -1,14 +1,38 @@
+using Microsoft.EntityFrameworkCore;
 using MockTestCs.Entities;
+using MockTestCs.Services.Password;
 
 namespace MockTestCs.Features.CreateUser;
 
 public class CreateUserUseCase(
-    MockTestCsDbContext ctx
+    MockTestCsDbContext ctx,
+    IPasswordServices password
 )
 {
     public async Task<Result<CreateUserResponse>> Do(CreateUserPayload payload)
     {
-        
-    }
+        var exist = await ctx.Users
+            .AnyAsync(u => u.Username == payload.Username || u.Email == payload.Email);
 
+        if (exist)
+            return Result<CreateUserResponse>.Fail("This login is already in use");
+        
+        var user = new User{
+            Username = payload.Username,
+            Email = payload.Email,
+            Description = payload.Description,
+            Password = password.Hash(payload.Password)
+        };
+
+        ctx.Users.Add(user);
+        await ctx.SaveChangesAsync();
+
+        return Result<CreateUserResponse>.Success( new CreateUserResponse(
+                user.ID,
+                user.Username,
+                user.Email,
+                user.Description
+            )
+        );
+    }
 }
