fdd338c934ee1ce48481926d9e481ce9411be828
Anna Guerra <annaguerracastro@gmail.com>
2025-09-08 12:54:54 -0300

    login use case + dtos

--- MockTestCs/Features/Login/LoginResponse.cs
+++ MockTestCs/Features/Login/LoginResponse.cs
@@ -1,3 +1,5 @@
 namespace MockTestCs.Features.Login;
 
-public record LoginResponse;
+public record LoginResponse(
+    string Token
+);
--- MockTestCs/Features/Login/LoginUseCase.cs
+++ MockTestCs/Features/Login/LoginUseCase.cs
@@ -1,6 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MockTestCs.Entities;
+using MockTestCs.Services.JWT;
+using MockTestCs.Services.Password;
+
 namespace MockTestCs.Features.Login;
 
 public class LoginUseCase(
     MockTestCsDbContext ctx, 
-    I
+    IPasswordServices password,
+    IJWTService jwt
 )
+{
+    public async Task<Result<LoginResponse>> Do(LoginPayload payload)
+    {
+        var user = await ctx.Users
+            .FirstOrDefaultAsync(u => u.Email == payload.Login || u.Username == payload.Login);
+
+        var passwordMatch = password.Compare(payload.Password, user.Password);
+
+        if (!passwordMatch || user is null)
+            return Result<LoginResponse>.Fail("User not found");
+
+        var jwtService = new JWTService();
+        var token = jwtService.CreateToken(new UserToLoginDto(
+            user.ID, user.Username
+            )
+        );
+
+        await ctx.SaveChangesAsync();
+        return Result<LoginResponse>.Success(new LoginResponse(token));
+    }
+}
