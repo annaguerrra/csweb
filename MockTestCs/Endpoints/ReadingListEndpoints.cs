9d54f895993b12f44f53fb6597127d1913f23120
Anna Guerra <annaguerracastro@gmail.com>
2025-09-09 22:09:19 -0300

    create list endpoints

--- MockTestCs/Endpoints/ReadingListEndpoints.cs
+++ MockTestCs/Endpoints/ReadingListEndpoints.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using MockTestCs.Features.AddToList;
+using MockTestCs.Features.CreateList;
 using MockTestCs.Features.DeleteFromList;
 
 namespace MockTestCs.Endpoints;
@@ -25,7 +26,7 @@ public static class ReadingListEndpoints
             var userid = Guid.Parse(claim.Value);
             var readingListID = Guid.Parse(listid);
             var historyid = Guid.Parse(history);
-            
+
 
             var payload = new AddToListPayload(userid, historyid, readingListID);
 
@@ -66,5 +67,30 @@ public static class ReadingListEndpoints
             };
 
         }).RequireAuthorization();
+        
+        app.MapPost("createlist", async(
+            string userid,
+            HttpContext http,
+            string Title,
+            [FromServices] CreateListUseCase useCase
+        ) =>
+        {
+            var claim = http.User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim is null)
+                return Results.BadRequest("User not found");
+
+            var userId = Guid.Parse(claim.Value);
+
+            var payload = new CreateListPayload(userId, Title);
+            var result = await useCase.Do(payload);
+
+            return( result.IsSuccess, result.Reason) switch
+            {
+                (false, "User not found") => Results.BadRequest(),
+                (false, _) => Results.BadRequest(),
+                (true, _) => Results.Ok()
+            };
+        });
     }
 }
